3d6edc7d576972ffe8d0745c35deaa2730ca0341
Sarvagya Ranjan <sarvagyar@gmail.com>
2022-03-06 19:33:24 +0530

    tailwind css site

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/index.html'
+  ],
+  theme: {
+    extend: {},
+    // backgroundImage: {
+    //   'hero-pattern': "url('/img/hero-pattern.svg')",
+    //   'footer-texture': "url('/img/footer-texture.png')"
+    // },
+    // prefix: 'tw-',
+    plugins: [],
+  }
+}
